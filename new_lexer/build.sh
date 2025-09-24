5dfa16133d8a0fb6b75d89330fce0cfdc9ef6d05
yukii <lichtestv@gmail.com>
2025-09-24 21:41:50 +0200

    not going to rewrite lexer yet, not sure if it is worth it, I got far into it, but I need to test it more. Only around processing 500 bytes, not sure if the gains are there yet, I will test it more in a different repo. Will implement zero copy strings and improve branching as well as locality first

--- new_lexer/build.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build/bin/
-
-nasm -g -f elf64 -F dwarf src/whisker/stringd/ws_strnlen.asm -o build/ws_strnlen.o
-
-clang -O0 -g -mavx2 build/ws_strnlen.o src/lexer.c src/main.c -o build/bin/lexer
--- new_lexer/src/whisker/stringd/stringd.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRINGD_H
-#define STRINGD_H
-
-#include <stddef.h>
-
-extern size_t ws_strnlen(const char* p, size_t n);
-
-#endif // !STRINGD_H
